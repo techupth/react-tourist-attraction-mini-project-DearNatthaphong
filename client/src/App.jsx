1a227945b4d924a61b4d0713e6731eea0a24ea2b
LAPTOP-F25A12VH\Dear <natthaphong.ea@gmail.com>
2024-06-12 09:35:20 +0700

    chore: config tailwind CSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
-import "./App.css";
+import './App.css';
 
 function App() {
-  return <div className="App">{/* Start coding here */}</div>;
+  return (
+    <div className="App">
+      {/* Start coding here */}
+      <h1 className="text-3xl font-bold underline text-red-500 text-center">
+        Hello world!
+      </h1>
+    </div>
+  );
 }
 
 export default App;
